2c0f92c0ea66aa8c317f36af685edb7968f4383c
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-01-02 12:25:32 +0100

    Upload du projet python

---
+++ jour1/job03/job03.py
@@ -0,0 +1,10 @@
+# Job 1
+print(10 + 3)
+print(10 * 3)
+print(10 % 3)
+print(10 - 3)
+print(10 / 3)
+print(10 // 3)
+
+# Job 2
+print(10 + 3)
---
+++ jour1/pour-aller-plus-loin/pour-aller-plus-loin.py
@@ -0,0 +1,7 @@
+txt = "Je suis une phrase test, ceci est un essai."
+
+if "e" in txt:
+    print("cette chaîne de caractères contient la lettre e.")
+
+
+else: print("cette chaîne de caractères ne contient pas la lettre e.")
