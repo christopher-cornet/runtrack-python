4247a4fb7fdc283c89eaea8842923af158539a4b
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-01-03 17:03:51 +0100

    Add files via upload

--- jour2/pour-aller-plus-loin/pour-aller-plus-loin.py
+++ jour2/pour-aller-plus-loin/pour-aller-plus-loin.py
@@ -1,10 +1,14 @@
 def fonction(a, b, c):
+    if a <= b+c and b <= a+c and c <= a+b:
+        print("Constructible")
+    else:
+        print("Non constructible")
     if a == b == c:
         print("Triangle équilatéral")
     elif a==b or b==c or c==a:
         print("Triangle isocèle")
     elif (a**2 + b**2 == c**2) or (a**2 + c**2 == b**2) or (b**2 + c**2 == a**2):
-        print("triangle rectangle")
+        print("Triangle rectangle")
     else:
         print("Triangle quelconque")
 
@@ -12,3 +16,4 @@ fonction(25, 25, 25)
 fonction(33, 66, 33)
 fonction(5, 12, 13)
 fonction(10, 20, 30)
+fonction(10, 80, 30)
