63104b7a1a770ed008ab2a0981414963e99edfd8
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-01-04 17:14:58 +0100

    Add files via upload

--- jour3/pour-aller-plus-loin/pour-aller-plus-loin.py
+++ jour3/pour-aller-plus-loin/pour-aller-plus-loin.py
@@ -1,3 +1,5 @@
-print("Ecrivez un texte à inverser:")
-txt = ("Développeur Python")[::-1]
-print(txt)
+def script(txt):
+    texteinverse = txt[::-1]
+    print(texteinverse)
+
+script("Développeur Python")
